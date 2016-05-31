d597912f513554f29d1b38bdd093818b3aa23638
Armao Thao <software.exploration@gmail.com>
2016-05-31 01:06:11 -0700

    Modified files to support voting options and results.

--- polls/views.py
+++ polls/views.py
@@ -1,34 +1,77 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.template import loader
+from django.http import Http404
 
-from .models import Question
+from .models import Question, Choice
+from django.db.models import F
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    context = {'latest_question_list': latest_question_list}
+    response = render(request, 'polls/index.html', context)
     print("\n\nRequest: " + str(request))
     print("latest_question_list: " + str(latest_question_list))
-    print("template: " + str(template))
-    print("context: " + str(template))
-    httpresp = template.render(context, request)
-    print("http response: " + str(httpresp))
-    return HttpResponse(httpresp)
+    print("context: " + str(context))
+    print("response: " + str(response))
+    return response
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     print("\n\nRequest: " + str(request))
+#     print("latest_question_list: " + str(latest_question_list))
+#     print("template: " + str(template))
+#     print("context: " + str(template))
+#     httpresp = template.render(context, request)
+#     print("http response: " + str(httpresp))
+#     return HttpResponse(httpresp)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     # return HttpResponse("You're looking at question %s." % question_id)
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
 
 
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # F takes the current value of the datase and increments it.
+        # This prevents race condition when multiple thread access data.
+        # selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
