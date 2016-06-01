229d328105a2e6156c5797ef589a1eab017b79b5
Armao Thao <software.exploration@gmail.com>
2016-05-31 23:28:45 -0700

    - Completed Django tutorial 4 (Django generic views) 	- db.sqlite3 	- polls/urls.py 	- polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,26 @@ from django.http import Http404
 
 from .models import Question, Choice
 from django.db.models import F
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 # Create your views here.
